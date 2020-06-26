86e8a78b239ee74b8461dbb9e570a11945623982
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-26 15:36:24 +0200

    adding header with viaplay log adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header'
 import SeriesGrid from './components/SeriesGrid'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
+    <>
+    <Header />
     <SeriesGrid />
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg" alt="viaplay-logo"></img>
+    </div>
+  )
+}
+
+export default Header
